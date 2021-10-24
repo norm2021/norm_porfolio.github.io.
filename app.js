26153496ebd8d2dd50f30dd0ce19f6d96c5284be
norm2021 <89789598+norm2021@users.noreply.github.com>
2021-10-24 14:35:50 +0100

    email

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const navSlide = () =>{
     const burger = document.querySelector('.burger');
     const nav_list = document.querySelector('.nav-list');
-    const change_nav_height = document.querySelector('#Navigation');
     const nav_list_items = document.querySelectorAll('.nav-list li');
 
     burger.addEventListener('click', ()=>{
         nav_list.classList.toggle('nav-active');
-        change_nav_height.classList.toggle('nav-height');
-
+       
         nav_list_items.forEach((item, index) => {
             if(item.style.animation){
                 item.style.animation = '';
             }else{
-                item.style.animation = `nav-list-fade 0.5 ease forwards ${index / 2 + 1.5}s`
+                item.style.animation = `nav-list-fade 0.5 ease forwards ${index / 2 + 1.5}s`;
             }
         })
+
+        burger.classList.toggle('toggle');
     });
 }
 
