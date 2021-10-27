dcd0b6786c9a699312c2ef98ae006f99bc289802
norm2021 <89789598+norm2021@users.noreply.github.com>
2021-10-27 20:36:25 +0100

    nav bar non fixed

--- app.js
+++ app.js
@@ -18,4 +18,20 @@ const navSlide = () =>{
     });
 }
 
+$(document).ready(function() {
+  
+    $(window).scroll(function () {
+        //if you hard code, then use console
+        //.log to determine when you want the 
+        //nav bar to stick.  
+        console.log($(window).scrollTop())
+      if ($(window).scrollTop() > 752) {
+        $('#Navigation').addClass('nav-fixed');
+      }
+      if ($(window).scrollTop() < 752) {
+        $('#Navigation').removeClass('nav-fixed');
+      }
+    });
+  });
+
 navSlide();
